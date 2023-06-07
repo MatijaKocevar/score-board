0427894c1c7a07e95366e3a2f59886e53b223fe4
Matija Kočevar <matija.kocev@gmail.com>
2023-06-07 13:37:51 +0200

    update game, tests

--- src/component/ScoreBoard/ScoreBoard.test.tsx
+++ src/component/ScoreBoard/ScoreBoard.test.tsx
@@ -1,99 +1,102 @@
 import { fireEvent, render } from "@testing-library/react";
-import ScoreBoard from "./ScoreBoard";
 import "@testing-library/jest-dom";
 import App from "../../App";
-import {
-  expectedGamesOrder,
-  gamesOrderToEnter,
-  newGame,
-} from "../../utils/testData";
+import { expectedGamesOrder, gamesOrderToEnter } from "../../utils/testData";
 
 describe("ScoreBoard", () => {
-  it("renders the game correctly", () => {
-    const game = newGame;
+	it("Checks if new game is rendered correctly", () => {
+		const { container, getByText } = render(<App />);
+		// Enter new game values
+		const homeTeamInput = container.querySelector("#home-team__name-input");
+		const homeScoreInput = container.querySelector("#home-team__score-input");
+		const awayScoreInput = container.querySelector("#away-team__score-input");
+		const awayTeamInput = container.querySelector("#away-team__name-input");
 
-    const { getByText } = render(<ScoreBoard {...game} />);
+		if (homeTeamInput) fireEvent.change(homeTeamInput, { target: { value: "New Home Team" } });
+		if (homeScoreInput) fireEvent.change(homeScoreInput, { target: { value: "15" } });
+		if (awayScoreInput) fireEvent.change(awayScoreInput, { target: { value: "10" } });
+		if (awayTeamInput) fireEvent.change(awayTeamInput, { target: { value: "New Away Team" } });
 
-    const homeTeamName = getByText(game.homeTeam);
-    const homeTeamScore = getByText(game.homeScore);
-    const awayTeamScore = getByText(game.awayScore);
-    const awayTeamName = getByText(game.awayTeam);
+		// Click the "New Game" button
+		const newGameButton = getByText("New/Update Game");
+		fireEvent.click(newGameButton);
 
-    expect(homeTeamName).toBeInTheDocument();
-    expect(homeTeamScore).toBeInTheDocument();
-    expect(awayTeamScore).toBeInTheDocument();
-    expect(awayTeamName).toBeInTheDocument();
-  });
+		const homeTeamName = container.querySelector("#home-team__name");
+		const homeTeamScore = container.querySelector("#home-team__score");
+		const awayTeamScore = container.querySelector("#away-team__score");
+		const awayTeamName = container.querySelector("#away-team__name");
 
-  it("updates the ScoreBoard with new game values", () => {
-    const { getByText, container } = render(<App />);
+		expect(homeTeamName).toHaveTextContent("New Home Team");
+		expect(homeTeamScore).toHaveTextContent("0");
+		expect(awayTeamScore).toHaveTextContent("0");
+		expect(awayTeamName).toHaveTextContent("New Away Team");
+	});
 
-    // Enter new game values
-    const homeTeamInput = container.querySelector("#home-team__name-input");
-    const homeScoreInput = container.querySelector("#home-team__score-input");
-    const awayScoreInput = container.querySelector("#away-team__score-input");
-    const awayTeamInput = container.querySelector("#away-team__name-input");
+	it("updates the ScoreBoard with new game values", () => {
+		const { getByText, container } = render(<App />);
 
-    if (homeTeamInput)
-      fireEvent.change(homeTeamInput, { target: { value: "New Home Team" } });
-    if (homeScoreInput)
-      fireEvent.change(homeScoreInput, { target: { value: "15" } });
-    if (awayScoreInput)
-      fireEvent.change(awayScoreInput, { target: { value: "10" } });
-    if (awayTeamInput)
-      fireEvent.change(awayTeamInput, { target: { value: "New Away Team" } });
+		// Enter new game values
+		const homeTeamInput = container.querySelector("#home-team__name-input");
+		const homeScoreInput = container.querySelector("#home-team__score-input");
+		const awayScoreInput = container.querySelector("#away-team__score-input");
+		const awayTeamInput = container.querySelector("#away-team__name-input");
 
-    // Click the "New Game" button
-    const newGameButton = getByText("New Game");
-    fireEvent.click(newGameButton);
+		for (let i = 0; i < 2; i++) {
+			if (homeTeamInput) fireEvent.change(homeTeamInput, { target: { value: "New Home Team" } });
+			if (homeScoreInput) fireEvent.change(homeScoreInput, { target: { value: "15" } });
+			if (awayScoreInput) fireEvent.change(awayScoreInput, { target: { value: "10" } });
+			if (awayTeamInput) fireEvent.change(awayTeamInput, { target: { value: "New Away Team" } });
 
-    // Check if the updated game values are rendered in the ScoreBoard
-    const homeTeamName = getByText("New Home Team");
-    const homeTeamScore = getByText("15");
-    const awayTeamScore = getByText("10");
-    const awayTeamName = getByText("New Away Team");
+			// Click the "New Game" button
+			const newGameButton = getByText("New/Update Game");
+			fireEvent.click(newGameButton);
+		}
 
-    expect(homeTeamName).toBeInTheDocument();
-    expect(homeTeamScore).toBeInTheDocument();
-    expect(awayTeamScore).toBeInTheDocument();
-    expect(awayTeamName).toBeInTheDocument();
-  });
+		// Check if the updated game values are rendered in the ScoreBoard
+		const homeTeamName = getByText("New Home Team");
+		const homeTeamScore = getByText("15");
+		const awayTeamScore = getByText("10");
+		const awayTeamName = getByText("New Away Team");
 
-  it("checks the order of the list displayed", () => {
-    const { getByText, container } = render(<App />);
+		expect(homeTeamName).toBeInTheDocument();
+		expect(homeTeamScore).toBeInTheDocument();
+		expect(awayTeamScore).toBeInTheDocument();
+		expect(awayTeamName).toBeInTheDocument();
+	});
 
-    // Enter new game values
-    const homeTeamInput = container.querySelector("#home-team__name-input");
-    const homeScoreInput = container.querySelector("#home-team__score-input");
-    const awayScoreInput = container.querySelector("#away-team__score-input");
-    const awayTeamInput = container.querySelector("#away-team__name-input");
+	it("checks the order of the list displayed", () => {
+		const { getByText, container } = render(<App />);
 
-    gamesOrderToEnter.forEach((game) => {
-      if (homeTeamInput)
-        fireEvent.change(homeTeamInput, { target: { value: game.homeTeam } });
-      if (homeScoreInput)
-        fireEvent.change(homeScoreInput, { target: { value: game.homeScore } });
-      if (awayScoreInput)
-        fireEvent.change(awayScoreInput, { target: { value: game.awayScore } });
-      if (awayTeamInput)
-        fireEvent.change(awayTeamInput, { target: { value: game.awayTeam } });
+		// Enter new game values
+		const homeTeamInput = container.querySelector("#home-team__name-input");
+		const homeScoreInput = container.querySelector("#home-team__score-input");
+		const awayScoreInput = container.querySelector("#away-team__score-input");
+		const awayTeamInput = container.querySelector("#away-team__name-input");
 
-      // Click the "New Game" button
-      const newGameButton = getByText("New Game");
-      fireEvent.click(newGameButton);
-    });
+		for (let i = 0; i < 2; i++) {
+			gamesOrderToEnter.forEach((game) => {
+				if (homeTeamInput) fireEvent.change(homeTeamInput, { target: { value: game.homeTeam } });
+				if (homeScoreInput) fireEvent.change(homeScoreInput, { target: { value: game.homeScore } });
+				if (awayScoreInput) fireEvent.change(awayScoreInput, { target: { value: game.awayScore } });
+				if (awayTeamInput) fireEvent.change(awayTeamInput, { target: { value: game.awayTeam } });
 
-    const scores = container.querySelector("#scores-wrapper");
+				// Click the "New Game" button
+				const newGameButton = getByText("New/Update Game");
+				fireEvent.click(newGameButton);
+			});
+		}
 
-    scores?.childNodes.forEach((child, index) => {
-      expectedGamesOrder.forEach((game, gameIndex) => {
-        if (index === gameIndex) {
-          expect(child).toHaveTextContent(game.homeTeam);
-          expect(child).toHaveTextContent(game.homeScore);
-          expect(child).toHaveTextContent(game.awayScore);
-          expect(child).toHaveTextContent(game.awayTeam);
-        }
-      });
-    });
-  });
+		const scores = container.querySelector("#scores-wrapper");
+
+		scores?.childNodes.forEach((child, index) => {
+			expectedGamesOrder.forEach((game, gameIndex) => {
+				if (index === gameIndex) {
+					expect(child).toHaveTextContent(game.homeTeam);
+					expect(child).toHaveTextContent(game.homeScore);
+					expect(child).toHaveTextContent(game.awayScore);
+					expect(child).toHaveTextContent(game.awayTeam);
+				}
+			});
+		});
+	});
 });
--- src/component/ScoreBoard/ScoreBoard.tsx
+++ src/component/ScoreBoard/ScoreBoard.tsx
@@ -1,90 +1,91 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ScoreBoardStyle.scss";
 import { sortGames } from "../../utils/utils";
-import { expectedGamesOrder } from "../../utils/testData";
 
 export interface Game {
-  homeTeam?: string;
-  homeScore?: string;
-  awayTeam?: string;
-  awayScore?: string;
-  createdAt?: Date;
+	homeTeam?: string;
+	homeScore?: string;
+	awayTeam?: string;
+	awayScore?: string;
+	createdAt?: Date;
 }
 
 const ScoreBoard: React.FC<Game> = (game: Game) => {
-  const [games, setGames] = useState<Game[]>();
+	const [games, setGames] = useState<Game[]>();
 
-  useEffect(() => {
-    const isMissingProp =
-      game.awayTeam === "" ||
-      game.awayTeam === undefined ||
-      game.homeTeam === "" ||
-      game.homeTeam === undefined;
+	useEffect(() => {
+		const isMissingProp = game.awayTeam === "" || game.awayTeam === undefined || game.homeTeam === "" || game.homeTeam === undefined;
+		const gameExists = games?.some((gamePrev) => gamePrev.homeTeam === game.homeTeam && gamePrev.awayTeam === game.awayTeam);
+		const isMissingHomeTeam = game.homeTeam === "" || game.homeTeam === undefined;
+		const isMissingAwayTeam = game.awayTeam === "" || game.awayTeam === undefined;
 
-    const isGameDuplicate = games?.some(
-      (gamePrev) =>
-        gamePrev.homeTeam === game.homeTeam &&
-        gamePrev.awayTeam === game.awayTeam
-    );
+		if (isMissingProp) {
+			if (isMissingAwayTeam && !isMissingHomeTeam) alert(`Missing team name: "Away"`);
+			if (isMissingHomeTeam && !isMissingAwayTeam) alert(`Missing team name: "Home"`);
 
-    if (isGameDuplicate) {
-      alert("Game already exists");
-    } else if (!isMissingProp) {
-      setGames((g) => {
-        if (g) {
-          const newGame: Game = {
-            homeTeam: game.homeTeam,
-            homeScore: game.homeScore ?? "0",
-            awayTeam: game.awayTeam,
-            awayScore: game.awayScore ?? "0",
-            createdAt: new Date(),
-          };
+			return;
+		}
 
-          const newGames = [...g, newGame];
-          const sortedGames = sortGames(newGames);
+		if (gameExists) {
+			setGames((g) => {
+				const gameToUpdate = g?.find((gamePrev) => gamePrev.homeTeam === game.homeTeam && gamePrev.awayTeam === game.awayTeam);
 
-          return sortedGames;
-        } else {
-          return [game];
-        }
-      });
-    } else {
-      const isMissingHomeTeam =
-        game.homeTeam === "" || game.homeTeam === undefined;
-      const isMissingAwayTeam =
-        game.awayTeam === "" || game.awayTeam === undefined;
+				if (gameToUpdate) {
+					gameToUpdate.awayScore = game.awayScore ?? "0";
+					gameToUpdate.homeScore = game.homeScore ?? "0";
 
-      if (isMissingAwayTeam && !isMissingHomeTeam)
-        alert(`Missing team name: "Away"`);
+					if (g?.length) {
+						const newGames = [...g];
+						const sortedGames = sortGames(newGames);
 
-      if (isMissingHomeTeam && !isMissingAwayTeam)
-        alert(`Missing team name: "Home"`);
-    }
-  }, [game]);
+						return sortedGames;
+					}
+				}
+			});
+		} else
+			setGames((g) => {
+				const newGame: Game = {
+					homeTeam: game.homeTeam,
+					homeScore: "0",
+					awayTeam: game.awayTeam,
+					awayScore: "0",
+					createdAt: new Date(),
+				};
 
-  return (
-    <div id="scores-wrapper" className="scores-wrapper">
-      {games?.map((game, i) => {
-        return (
-          <div id={"game" + i} className="game" key={"game" + i}>
-            <div id="home-team__name" className="home-team__name">
-              {game.homeTeam}
-            </div>
-            <div id="home-team__score" className="home-team__score">
-              {game.homeScore}
-            </div>
-            <div> - </div>
-            <div id="away-team__score" className="away-team__score">
-              {game.awayScore}
-            </div>
-            <div id="away-team__name" className="away-team__name">
-              {game.awayTeam}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+				if (g?.length) {
+					const newGames = [...g, newGame];
+					const sortedGames = sortGames(newGames);
+
+					return sortedGames;
+				} else return [newGame];
+			});
+		// i don't want to react to games array changes
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [game]);
+
+	return (
+		<div id='scores-wrapper' className='scores-wrapper'>
+			{games?.map((game, i) => {
+				return (
+					<div id={"game" + i} className='game' key={"game" + i}>
+						<div id='home-team__name' className='home-team__name'>
+							{game.homeTeam}
+						</div>
+						<div id='home-team__score' className='home-team__score'>
+							{game.homeScore}
+						</div>
+						<div> - </div>
+						<div id='away-team__score' className='away-team__score'>
+							{game.awayScore}
+						</div>
+						<div id='away-team__name' className='away-team__name'>
+							{game.awayTeam}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default ScoreBoard;
